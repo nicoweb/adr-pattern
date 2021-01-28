70ad9077e92cc40f5d180500294d079fbe888bc2
Nicolas <nicolas.lefevre@meero.com>
2021-01-28 11:15:29 +0100

    remove abstractController

--- src/Controller/BlogPost/BlogPostListController.php
+++ src/Controller/BlogPost/BlogPostListController.php
@@ -6,21 +6,23 @@ namespace App\Controller\BlogPost;
 
 use App\Repository\BlogPostRepository;
 use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 #[Route('/post', name: 'blog_post_index', methods: ['GET'])]
-class BlogPostListController extends AbstractController
+class BlogPostListController
 {
     private PaginatorInterface $paginator;
     private BlogPostRepository $blogPostRepository;
+    private Environment $twig;
 
-    public function __construct(PaginatorInterface $paginator, BlogPostRepository $blogPostRepository)
+    public function __construct(PaginatorInterface $paginator, BlogPostRepository $blogPostRepository, Environment $twig)
     {
         $this->paginator = $paginator;
         $this->blogPostRepository = $blogPostRepository;
+        $this->twig = $twig;
     }
 
     public function __invoke(Request $request): Response
@@ -30,8 +32,8 @@ class BlogPostListController extends AbstractController
             $request->query->getInt('page', 1)
         );
 
-        return $this->render('blog_post/index.html.twig', [
+        return new Response($this->twig->render('blog_post/index.html.twig', [
             'blog_posts' => $posts,
-        ]);
+        ]));
     }
 }
